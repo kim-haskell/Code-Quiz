8351cf61c71834d2d02aed370a8d31eb0f716674
Kim Haskell <kim.haskell03@gmail.com>
2020-07-01 18:48:26 -0600

    adding questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if/else statement is enclosed with:",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ________ .",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed with:",
+        choices: ["commas", "curly brackets", "parentheses", "quotes"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool used during development and debugging is:",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    },
+    
+];
---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerID;
+
+
+
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var initalsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
+
+
+
+function startQuiz () {
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class");
+    timerID = setInterval(clockTick, 1000);
+    timerEl.textContent = time;
+    getQuestion();
+}
+function clockTick () {
+    time-- 
+    timerEl.textContent = time;
+    if (time <= 0){
+        quizEnd()
+    }
+}
+function getQuestion () {
+    var currentQuestion = questions[currentQuestionIndex];
+    var titleEl // finish
+    titleEl.textContent = currentQuestion.title;
+    choicesEl.innerHTML = "";
+    currentQuestion.choices.forEach(function(choice, i){
+        var choicenode = document.createElement("button");
+        choicenode.setAttribute("class", "choice");
+        choicenode.setAttribute("value", choice);
+        choicenode.textContent = choice;
+        choicenode.onclick = questionClick;
+        choicesEl.appendChild(choicenode);
+    })
+
+}
+function questionClick () {
+    
+}
+function quizEnd () {
+
+}
+
+startBtn.onclick = startQuiz;
